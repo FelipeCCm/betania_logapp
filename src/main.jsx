eabdd6db8ccb31bcc3557092edcfd67fff7a141e
FelipeCCM <felipeccmoura@gmail.com>
2026-03-18 22:47:12 -0300

    add timer

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { TimerProvider } from './contexts/TimerContext.jsx'
 import './App.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <TimerProvider>
+      <App />
+    </TimerProvider>
   </React.StrictMode>,
 )
